9ff11d1084fc9665d0a5f0d5052a37f87754c412
Sachin Krishna <sachinkrishna@ikm.gov.in>
2025-04-07 16:20:56 +0530

    authentication api completed.

--- src/main/java/com/snackify/repository/UserRepository.java
+++ src/main/java/com/snackify/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.snackify.repository;
 
-public interface UserRepository {
+import com.snackify.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
